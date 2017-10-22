5eadbed69cfba78384012a1df99234735b8669ed
alshin <dm.alshin@gmail.com>
2017-10-22 17:51:44 +0300

    Bootstraping and emoji model

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,7 +11,23 @@
 
 module.exports.bootstrap = function(cb) {
 
-  // It's very important to trigger this callback method when you are finished
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  // Emoji.create([{
+  //   text: '=)'
+  // }, {
+  //   text: ':('
+  // }, {
+  //   text: ';p'
+  // }]).exec(function(err, result) {
+  //   if (err) {
+  //     // handle the error case
+  //     cb();
+  //   }
+  //   else {
+  //     // It's very important to trigger this callback method when you are finished
+  //     // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+  //     cb();
+  //   }
+  // });
+
+cb();
 };
